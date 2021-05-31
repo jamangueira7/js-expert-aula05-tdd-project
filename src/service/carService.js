bef609441f82f4bfca3c6e342559085cedd94911
João Mangueira <jamjs7@gmail.com>
2021-05-30 22:27:05 -0300

    add os mocks para deixarem os testes mais fieis, add a primeira função do carService e criando a estrutura do test.

--- src/service/carService.js
+++ src/service/carService.js
@@ -4,6 +4,10 @@ class CarService {
     constructor({ cars }) {
         this.carRepository = new BaseRepository({ file: cars });
     }
+
+    getAvailableCar() {
+        return this.carRepository.find();
+    }
 }
 
 module.exports = CarService;
--- test/unitTests/carService.test.js
+++ test/unitTests/carService.test.js
@@ -1,8 +1,38 @@
-const { describe, it } = require('mocha');
+const { describe, it, before } = require('mocha');
 const sinon = require('sinon');
 
+const CarService = require('./../../src/service/carService');
+
+const { join } = require('path');
+const { chai } = require('chai');
+
+const carsDatabase  = join(__dirname, './../../database', "cars.json");
+
+const mocks = {
+    validCarCategory: require('./../mocks/valid-carCategory.json'),
+    validCar: require('./../mocks/valid-car.json'),
+    validCustomer: require('./../mocks/valid-customer.json'),
+};
+
 describe('CarService Suite Tests', () => {
-    it('test!', () => {
+    let carService = {};
+
+    before(() => {
+        carService = new CarService({
+            cars: carsDatabase
+        });
+    });
+
+    it('given a carCategory it should return an available car', async () => {
+        const car = mocks.validCar;
+        const carCategory = Object.create(mocks.validCarCategory);
+        carCategory.ids = [car.id];
+
+        const result = await carService.getAvailableCar(carCategory);
+
+        const expected = car;
+
+        expect(result).to.be.deep.equal(expected);
 
     });
 });
